1c1fccca40c5c5d4b5df15e3d62148cb15a13e32
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-06-17 01:49:16 +0600

    Functionality for add to cart added

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSLice from "./auth-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
     reducer: {
-        auth: authSLice.reducer
+        auth: authSLice.reducer,
+        cart: cartSlice.reducer
     }
 })
 
