fd495c742fbbfa0c19612a79fe70190f9058760d
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-06-17 03:35:27 +0600

    Firebase live databse connected.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notifications from "./components/Notifications";
+import { uiActions } from "./store/ui-slice";
+let isFirstRender = true;
 
 function App() {
-
+  const dispatch = useDispatch()
+  const notification = useSelector(state => state.ui.notification)
+  const cart = useSelector(state => state.cart)
+  useEffect(() => {
+    if (isFirstRender) return isFirstRender = false;
+    const sendRequest = async () => {
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: "Sending Request",
+        type: 'warning'
+      }))
+      const res = await fetch('https://redux-tutorial-with-database-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+      const data = await res.json();
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: "Request Sent",
+        type: 'success'
+      }))
+    }
+    sendRequest().catch(err => {
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: "Something Occured",
+        type: 'Error'
+      }))
+    })
+  }, [cart, dispatch])
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
 
   return (
     <div className="App">
+      {notification && <Notifications type={notification.type} message={notification.message}></Notifications>}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSLice from "./auth-slice";
 import cartSlice from "./cart-slice";
+import uiSlice from "./ui-slice";
 
 const store = configureStore({
     reducer: {
         auth: authSLice.reducer,
-        cart: cartSlice.reducer
+        cart: cartSlice.reducer,
+        ui: uiSlice.reducer
     }
 })
 
